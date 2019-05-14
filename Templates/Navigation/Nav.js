e6b2d44b4235fe5bf33d50940e482345b0a45fda
s310416 <48338967+s310416@users.noreply.github.com>
2019-05-14 23:35:28 +0930

    Java Scriptfor navigation
    Signed-off-by: s310416 <48338967+s310416@users.noreply.github.com>

--- Templates/Navigation/Nav.js
+++ Templates/Navigation/Nav.js
@@ -1,1 +1,10 @@
 
+function fun(){
+  var x = document.getElementById('main')
+  if(x.style.display === "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
