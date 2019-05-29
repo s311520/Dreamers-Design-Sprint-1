8dcfaa88ba722144eb4eaf2648d761375cb455e8
s310416 <48338967+s310416@users.noreply.github.com>
2019-05-30 02:12:26 +0930

    adding new style for data inside reference card which includes sliding image and acordion menu and uploading images that is used for tht page
    Signed-off-by: s310416 <48338967+s310416@users.noreply.github.com>

--- mvp.js
+++ mvp.js
@@ -17,3 +17,41 @@ function menu(){
     x.style.display = "block";
   }
 }
+
+var images = ['a.jpg', 'b.jpg', 'c.jpg','d.jpg', 'e.jpg'];
+
+    var x = 0;
+
+    var imgs = document.getElementById('img');
+
+    setInterval(slider, 3000);
+
+
+    function slider() {
+
+      if (x < images.length) {
+        x = x + 1;
+      } else {
+        x = 1;
+      }
+
+
+      imgs.innerHTML = "<img src=" + images[x - 1] + ">";
+
+
+    }
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
